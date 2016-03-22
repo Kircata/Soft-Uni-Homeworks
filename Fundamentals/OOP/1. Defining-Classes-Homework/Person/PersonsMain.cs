76944f95f1f6a0f8090560a8fe4ddbfd0fc3ba3b
Kircata <kironakumura@abv.bg>
2016-03-23 00:02:44 +0200

    Added defining classes homework

---
+++ Fundamentals/OOP/1. Defining-Classes-Homework/Person/PersonsMain.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Person
+{
+    class PersonsMain
+    {
+        static void Main(string[] args)
+        {
+            Person pesho = new Person("Pesho", 17);
+            Person gosho = new Person("Gosho", 17, "goshopi4a@abv.bg");
+            Console.WriteLine(pesho.ToString());
+            Console.WriteLine(gosho.ToString());
+        }
+    }
+}
