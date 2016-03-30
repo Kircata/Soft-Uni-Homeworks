066967db2cf40f29dd936a47c6918c731ccd3e71
Kircata <kironakumura@abv.bg>
2016-03-31 00:08:40 +0300

    Added code tuning and optimization homework

---
+++ Fundamentals/High-Quality Code/07. Code-Tuning-And-Optimization-Homework/3.CompareSortingAlgorithms/Application.cs
@@ -0,0 +1,36 @@
+﻿namespace _3.CompareSortingAlgorithms
+{
+    using System;
+    using System.Linq;
+    using System.Diagnostics;
+
+    public class Application
+    {
+        public static void Main()
+        {
+            int[] numbers = GenerateRandomNums(10);
+            int[] oldNumbers = new int[numbers.Length];
+            Array.Copy(numbers, oldNumbers, numbers.Length);
+
+            Console.WriteLine("Insertion sort:");
+            SortingAlgorithms.InsertionSort(numbers);
+            Array.Copy(oldNumbers, numbers, oldNumbers.Length);
+
+            Console.WriteLine("Selection sort:");
+            SortingAlgorithms.SelectionSort(numbers);
+            Array.Copy(oldNumbers, numbers, oldNumbers.Length);
+        }
+
+        private static int[] GenerateRandomNums(int length)
+        {
+            int[] nums = new int[length];
+            Random rnd = new Random();
+            for (int i = 0; i < length; i++)
+            {
+                nums[i] = rnd.Next(-1000, 1000);
+            }
+
+            return nums;
+        }
+    }
+}
---
+++ Fundamentals/High-Quality Code/07. Code-Tuning-And-Optimization-Homework/3.CompareSortingAlgorithms/SortingAlgorithms.cs
@@ -0,0 +1,57 @@
+﻿namespace _3.CompareSortingAlgorithms
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public static class SortingAlgorithms
+    {
+        public static void InsertionSort(int[] arr)
+        {
+            for (int index = 1; index < arr.Length; index++)
+            {
+                int currentElement = arr[index];
+                int currentIndex = index - 1;
+                do
+                {
+                    if (currentElement < arr[currentIndex])
+                    {
+                        int oldValue = arr[currentIndex];
+                        arr[currentIndex] = currentElement;
+                        arr[currentIndex + 1] = oldValue;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    currentIndex--;
+                } while (currentIndex >= 0);
+            }
+        }
+
+        public static void SelectionSort(int[] arr)
+        {
+            for (int indexToPutIn = 0; indexToPutIn < arr.Length - 1; indexToPutIn++)
+            {
+                int smallestElement = int.MaxValue;
+                int index = 0;
+                for (int i = 0 + indexToPutIn; i < arr.Length; i++)
+                {
+                    int currentElement = arr[i];
+                    if (currentElement < smallestElement)
+                    {
+                        smallestElement = currentElement;
+                        index = i;
+                    }
+                }
+
+                int oldValue = arr[indexToPutIn];
+                arr[indexToPutIn] = smallestElement;
+                arr[index] = oldValue;
+            }
+        }
+
+    }
+}
