51e6e1ef4f0ecc742f0a97e5e918f5730b8ccc03
Kircata <kironakumura@abv.bg>
2016-04-04 20:27:04 +0300

    Added refactoring homework

---
+++ Fundamentals/High-Quality Code/10. Refactoring-Homework/MatrixLogic.cs
@@ -0,0 +1,78 @@
+﻿namespace Matrix
+{
+    public class MatrixLogic
+    {
+        private const int RotationTimes = 8;
+        private static int[] DirectionsX = { 1, 1, 1, 0, -1, -1, -1, 0 };
+        private static int[] DirectionsY = { 1, 0, -1, -1, -1, 0, 1, 1 };
+
+        public static void Change(ref int dx, ref int dy)
+        {
+            int cd = 0;
+            for (int count = 0; count < RotationTimes; count++)
+            {
+                if (DirectionsX[count] == dx && DirectionsY[count] == dy)
+                {
+                    cd = count;
+                    break;
+                }
+            }
+
+            if (cd == 7)
+            {
+                dx = DirectionsX[0];
+                dy = DirectionsY[0];
+                return;
+            }
+
+            dx = DirectionsX[cd + 1];
+            dy = DirectionsY[cd + 1];
+        }
+
+        public static bool Check(int[,] arr, int x, int y)
+        {
+            for (int i = 0; i < RotationTimes; i++)
+            {
+                if (x + DirectionsX[i] >= arr.GetLength(0) 
+                    || x + DirectionsX[i] < 0)
+                {
+                    DirectionsX[i] = 0;
+                }
+
+                if (y + DirectionsY[i] >= arr.GetLength(0) 
+                    || y + DirectionsY[i] < 0)
+                {
+                    DirectionsY[i] = 0;
+                }
+            }
+
+            for (int i = 0; i < RotationTimes; i++)
+            {
+                if (arr[x + DirectionsX[i], y + DirectionsY[i]] == 0)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static void FindCell(int[,] arr, out int x, out int y)
+        {
+            x = 0;
+            y = 0;
+            for (int row = 0; row < arr.GetLength(0); row++)
+            {
+                for (int col = 0; col < arr.GetLength(1); col++)
+                {
+                    if (arr[row, col] == 0)
+                    {
+                        x = row;
+                        y = col;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Fundamentals/High-Quality Code/10. Refactoring-Homework/MatrixMain.cs
@@ -0,0 +1,102 @@
+﻿namespace Matrix
+{
+    using System;
+
+    public class MatrixMain
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter a positive number ");
+            string input = Console.ReadLine();
+            int number;
+            while (!int.TryParse(input, out number) || number < 0 || number > 100)
+            {
+                Console.WriteLine("You haven't entered a correct positive number");
+                input = Console.ReadLine();
+            }
+
+            int[,] matrix = new int[number, number];
+            int step = number;
+            int value = 1;
+            int row = 0;
+            int col = 0;
+            int directionX = 1;
+            int directionY = 1;
+            while (true)
+            {
+                matrix[row, col] = value;
+
+                if (!MatrixLogic.Check(matrix, row, col))
+                {
+                    break;
+                }
+
+                var outOfBoundries = IsOutOfBoundries(row, directionX, number, col, directionY, matrix);
+
+                while (!outOfBoundries)
+                {
+                    MatrixLogic.Change(ref directionX, ref directionY);
+                    outOfBoundries = IsOutOfBoundries(row, directionX, number, col, directionY, matrix);
+                }
+
+                row += directionX;
+                col += directionY;
+                value++;
+            }
+
+            PrintMatrix(matrix);
+
+            MatrixLogic.FindCell(matrix, out row, out col);
+            if (row != 0 && col != 0)
+            { 
+                directionX = 1;
+                directionY = 1;
+                // TODO: Refactor into method
+                while (true)
+                { 
+                    matrix[row, col] = value;
+                    if (!MatrixLogic.Check(matrix, row, col))
+                    {
+                        break;
+                    }
+
+                    while ((row + directionX >= number || row + directionX < 0 || col + directionY >= number || col + directionY < 0 || matrix[row + directionX, col + directionY] != 0))
+                    {
+                        MatrixLogic.Change(ref directionX, ref directionY);
+                    }
+
+                    row += directionX;
+                    col += directionY;
+                    value++;
+                }
+            }
+
+            PrintMatrix(matrix);
+        }
+
+        private static void PrintMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write("{0,3}", matrix[row, col]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static bool IsOutOfBoundries(int row, int directionX, int number, int col, int directionY, int[,] matrix)
+        {
+            bool outOfBoundries =
+                row + directionX >= number ||
+                row + directionX < 0 ||
+                col + directionY >= number ||
+                col + directionY < 0 ||
+                matrix[row + directionX, col + directionY] != 0;
+
+            return outOfBoundries;
+        }
+    }
+}
